73895a020f789eb253e0ff8b7b1e8f6ba81b3cf1
Cn-coding <63557513+Cn-coding@users.noreply.github.com>
2024-01-29 22:02:03 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# This script is used to double the power values in a .gpx file.
+
+# Write file name here, include the .gpx extension
+FILENAME = "Cycling - Copy.gpx"
+
+# Creates new file name
+NEW_FILENAME = FILENAME[:-4] + "_doubled.gpx"
+
+
+# Doubles a single power value
+def double_power(line):
+    if "<power>" in line and "</power>" in line:
+        start_index = line.index("<power>") + 7
+        end_index = line.index("</power>")
+        power = line[start_index:end_index]
+        if power != "":
+            power = str(int(power) * 2)
+            line = line[:start_index] + power + line[end_index:]
+    return line
+
+
+# Reads and doubles power values
+with open(FILENAME, "r") as f:
+    lines = [double_power(x) for x in f.readlines()]
+    f.close()
+
+# Writes new file with doubled power values
+with open(NEW_FILENAME, "w") as f:
+    f.writelines(lines)
+    f.close()
